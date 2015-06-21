13366aad4d72b136b84efba91a172e6c0443e03c
Brett Binnersley <brettbinnersley14@gmail.com>
2015-06-21 15:32:55 -0700

    Setup with angular

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,14 @@
-angular.module('userApp', []);
+angular.module('userApp', [])
+.controller('mainController', function(){
+
+  var vm = this;
+
+  // basic variable to display
+  vm.message ="SENG 299 - Group 6 tutorial angular"
+
+  // a list of students that will be displayed on the home page
+  vm.students = [
+    {first: "David", last: "Johnson"},
+    {first: "Ernest", last: "Aaron"}
+  ];
+});
