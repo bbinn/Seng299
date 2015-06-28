8edb0151552d8ed4c64b7bd13dcddaa261668c5e
rossgb <r0554613@gmail.com>
2015-06-28 02:02:03 -0700

    initial commit of schedule page, includes working date picker, and a table of booths

--- public/app/controllers/scheduleCtrl.js
+++ public/app/controllers/scheduleCtrl.js
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 angular.module('userApp').controller('ScheduleController', ['$scope', '$http', 'ngDialog', function($scope, $http, ngDialog) {
   var vm = this;
   vm.date = new Date();
   vm.booths = [];
+=======
+angular.module('userApp').controller('ScheduleController', ['$scope', '$http', function($scope, $http) {
+  var vm = this;
+  vm.date = new Date();
+>>>>>>> initial commit of schedule page, includes working date picker, and a table of booths
   vm.lunchBooths = [];
   vm.produceBooths = [];
   vm.merchBooths = [];
@@ -12,6 +18,7 @@ angular.module('userApp').controller('ScheduleController', ['$scope', '$http', '
   }
 
   vm.repopulate = function() {
+<<<<<<< HEAD
     //getbooths(vm.day)
 
     for (var i = 0; i < 3; i++) {
@@ -29,19 +36,40 @@ angular.module('userApp').controller('ScheduleController', ['$scope', '$http', '
     for (var i = 0; i < 10; i++) {
       vm.merchBooths[i] = {name: "+ Book this booth", id: i}
     }
+=======
+    for (var i = 0; i < 6; i++) {
+      vm.lunchBooths[i] = {name: "+ Book this booth", id: "book/"+i}
+    }
+    for (var i = 0; i < 8; i++) {
+      vm.produceBooths[i] = {name: "+ Book this booth", id: "book/"+i}
+    }
+    for (var i = 0; i < 10; i++) {
+      vm.merchBooths[i] = {name: "+ Book this booth", id: "book/"+i}
+    }
+    //TEMP CODE
+    vm.lunchBooths[1] = {name: "Rocking Moroccan Deli", id: "booth/12"}
+    //END TEMP
+>>>>>>> initial commit of schedule page, includes working date picker, and a table of booths
   }
 
 
   vm.changeDay = function(newDay) {
     var oldDay = vm.date.getDay();
     var deltaDay = newDay-oldDay;
+<<<<<<< HEAD
     vm.date.setDate(vm.date.getDate() + deltaDay);
     vm.day = vm.date.getDate();
     vm.repopulate();
+=======
+    vm.day=days[newDay];
+    vm.date.setDate(vm.date.getDate() + deltaDay);
+    vm.day = vm.date.getDate();
+>>>>>>> initial commit of schedule page, includes working date picker, and a table of booths
   };
 
   vm.nextWeek = function() {
     vm.date.setDate(vm.date.getDate() + 7);
+<<<<<<< HEAD
     vm.repopulate();
   }
   vm.prevWeek = function() {
@@ -56,6 +84,11 @@ angular.module('userApp').controller('ScheduleController', ['$scope', '$http', '
       appendTo: '.dialogClass'
     });
 
+=======
+  }
+  vm.prevWeek = function() {
+    vm.date.setDate(vm.date.getDate() - 7);
+>>>>>>> initial commit of schedule page, includes working date picker, and a table of booths
   }
 
   vm.repopulate();
