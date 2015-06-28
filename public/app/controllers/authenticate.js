1e0d066109877c21b9999d8573f432d9f558e905
Brett Binnersley <brettbinnersley14@gmail.com>
2015-06-28 15:04:38 -0700

    Added jquery. Working on getting ajax routes setup

--- public/app/controllers/authenticate.js
+++ public/app/controllers/authenticate.js
@@ -3,9 +3,26 @@ function tryLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    sendCommand('login', {
-      username: username,
-      password: password
-    })
+    sendCommand(
+      'login',
+      {
+        username: username,
+        password: password
+      },
+      function(error, message) {
+        if(error !== null){
+          //Error occured. Handle it here
+          console.log(error);
+        }
+        else
+        {
+          //Success - do successful login here
+        }
+      }
+    );
+}
+
+// TODO: This
+function trySignup() {
 
 }
--- public/app/controllers/server.js
+++ public/app/controllers/server.js
@@ -1,7 +1,22 @@
 
 // Command: Command to send (IE: Login, bookBoth etc)
 // Body: Arguments to send to the server inside the body of the message
+// Callback(err, message): Callback function taking 2 arguments (will be envoked once the server)
+// Responds back with a message to the client.
 
-function sendCommand(command, body) {
-    console.log(command);
+function sendCommand(command, body, callback) {
+
+    ($).ajax(
+      {
+        url: 'api/' + command,
+        type: "POST",
+        data: { body: JSON.stringify(body) },
+        success: function (data, status, xhr){
+          return callback( null, data )
+        } ,
+        error: function (xhr, status, err) {
+          return callback( err );
+        }
+      }
+    );
 }
