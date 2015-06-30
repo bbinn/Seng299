60866bf6383ed67c2ca06b0866269198e01ccc3a
Brett Binnersley <brettbinnersley14@gmail.com>
2015-06-29 21:48:16 -0700

    Booking booth setup

--- app/controllers/booth.js
+++ app/controllers/booth.js
@@ -1,4 +1,4 @@
-var Account       = require('../models/account');
+var Booth       = require('../models/booth');
 var utils         = require('../utils');
 
 var BoothController;
@@ -9,6 +9,11 @@ BoothController = (function() {
 
 
   // API Call
+  // Args:
+  // timeslot: Date (UTS)
+  // boothNumber: (Integer, 0-Max num booths)
+  // type: String (booth type)
+  // description: String (optional)
   BoothController.book = function(req, res, accountInformation) {
     if(accountInformation == null) // No User
     {
@@ -23,7 +28,52 @@ BoothController = (function() {
       });
     }
     body = utils.safeParse(req.body.body);
+
     // Todo: check to see if booth is already booked. If yes -> throw an error. Else: Book it.
+    var time = body.timeslot;
+    var boothNumber = body.boothNumber;
+    var type = body.boothType;
+    var description = body.description;
+    if(time == null || boothNumber == null || type == null) {
+      return res.status(400).send({
+        error: "Missing part of the body"
+      });
+    }
+
+    Booth.find({
+      timeSlot: time,
+      boothNumber: boothNumber
+    })
+    .exec(function (err, docs) {
+      //Find to see if this booth is already booked
+      if(err) {
+        return res.status(500).send({});
+      }
+      if(docs.length != 0){
+        return res.status(400).send({
+          error: "This booth is already booked"
+        });
+      }
+
+      //Book this booth - (Save it into the database)
+      var booth = new Booth();
+      booth.timeSlot = time;
+      booth.vendorId = accountInformation._id;
+      booth.boothType = type;
+      booth.boothNumber = boothNumber;
+      booth.description = (description || "");
+
+      return booth.save( function(err){
+        if(err) {
+          return res.status(400).send({error: err});
+        }
+        else
+        {
+          return res.status(200).send({});
+        }
+      });
+
+    });
   }
 
   return BoothController;
--- app/models/booth.js
+++ app/models/booth.js
@@ -7,6 +7,7 @@ var BoothSchema = new Schema({
   timeSlot:     { type: Date, default: Date.now, required: true, select: true, index: true },
   vendorId:     { type: Number, required: true, select: true, index: true },
   boothType:    { type: String, required: true, select: true},
+  boothNumber:  { type: Number, required: true, select: true},
   description:  { type: String, required: true, select: true }
 
 });
