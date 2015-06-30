e6db90749437cc177b93c2efcfc13c78912f6a71
Brett Binnersley <brettbinnersley14@gmail.com>
2015-06-29 17:22:00 -0700

    Created booth & follower models

---
+++ app/models/booth.js
@@ -0,0 +1,16 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+
+var BoothSchema = new Schema({
+
+  timeSlot:     { type: Date, default: Date.now, required: true, select: true, index: true },
+  vendorId:     { type: Number, required: true, select: true, index: true },
+  boothType:    { type: String, required: true, select: true},
+  description:  { type: String, required: true, select: true }
+
+});
+
+
+module.exports = mongoose.model('Booth', BoothSchema);
+
