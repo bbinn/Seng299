6e055aa9e7079bd40716541967426ed2ee6ae49e
Brett Binnersley <brettbinnersley14@gmail.com>
2015-07-06 03:24:03 -0700

    Fixed angular bug on account page

--- public/app/controllers/profileCtrl.js
+++ public/app/controllers/profileCtrl.js
@@ -24,16 +24,17 @@ angular.module('userApp').controller('profileController', ['$scope', '$http', '$
 	// default profile picture and banner
 	vm.genericProfileImage = $sce.trustAsResourceUrl('../../assets/generic_profile.png');
 	vm.genericBannerImage = $sce.trustAsResourceUrl('../../assets/generic_banner.jpg');
-	
-	vm.userName = activeUser.name;
+
+	var user = activeUser || {};
+	vm.userName = user.name;
 
 	// user has the ability to book booths
-	if (activeUser.accountType == "vendor") {
+	if (user.accountType == "vendor") {
 		vm.canBook = true;
 	}
-	
+
 	vm.getActiveBooths = function() {
-		$http.post('api/getbooths', {body: JSON.stringify({ vendorId: activeUser._id})})
+		$http.post('api/getbooths', {body: JSON.stringify({ vendorId: user._id})})
 			.success(function(data, status, headers, config) {
 				console.log(data.docs);
 				for (var i = 0; i < data.docs.length; i++) {
@@ -56,6 +57,6 @@ angular.module('userApp').controller('profileController', ['$scope', '$http', '$
 	// need to add a way to add a description associated with the user
 	vm.desc = "Yes, shrubberies are my trade. I am a shrubber. My name is Roger the Shrubber. I arrange, design, and sell shrubberies. Yes, shrubberies are my trade. I am a shrubber. My name is Roger the Shrubber. I arrange, design, and sell shrubberies.";
 
-/* END TEST CODE */ 
+/* END TEST CODE */
 
 }]);
--- public/app/controllers/upload.js
+++ public/app/controllers/upload.js
@@ -6,39 +6,12 @@ angular.module('userApp')
     withCredentials: true
   });
 
-
-  uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-    console.info('onWhenAddingFileFailed', item, filter, options);
-  };
   uploader.onAfterAddingFile = function(fileItem) {
     fileItem.upload();
   };
-  uploader.onAfterAddingAll = function(addedFileItems) {
-    console.info('onAfterAddingAll', addedFileItems);
-  };
-  uploader.onBeforeUploadItem = function(item) {
-    console.info('onBeforeUploadItem', item);
-  };
-  uploader.onProgressItem = function(fileItem, progress) {
-    console.info('onProgressItem', fileItem, progress);
-  };
-  uploader.onProgressAll = function(progress) {
-    console.info('onProgressAll', progress);
-  };
-  uploader.onSuccessItem = function(fileItem, response, status, headers) {
-    console.info('onSuccessItem', fileItem, response, status, headers);
-  };
-  uploader.onErrorItem = function(fileItem, response, status, headers) {
-    console.info('onErrorItem', fileItem, response, status, headers);
-  };
-  uploader.onCancelItem = function(fileItem, response, status, headers) {
-    console.info('onCancelItem', fileItem, response, status, headers);
-  };
+
   uploader.onCompleteItem = function(fileItem, response, status, headers) {
-    console.info('onCompleteItem', fileItem, response, status, headers);
-  };
-  uploader.onCompleteAll = function() {
-    console.info('onCompleteAll');
+    console.log("Uploaded: " + response.file);
   };
 
 
